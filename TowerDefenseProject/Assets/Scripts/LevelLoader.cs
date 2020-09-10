92c06d76a79321b8f1a6cb52db4eee4d46fa1e4a
Mateusz Antczak <mati98848@gmail.com>
2020-09-10 12:40:11 +0200

    Loading screen and start scene on placeholders

---
+++ TowerDefenseProject/Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    [SerializeField] float timeToWait = 2f;
+    int currentSceneIndex;
+    int NextScene
+    {
+        get
+        {
+            return currentSceneIndex + 1;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        if (currentSceneIndex == 0)
+        {
+            StartCoroutine(LoadSceneAfterTime(NextScene, timeToWait));
+        }
+    }
+
+    private IEnumerator LoadSceneAfterTime(int buildIndex, float delayInSeconds)
+    {
+        yield return new WaitForSeconds(delayInSeconds);
+        SceneManager.LoadScene(buildIndex);
+    }
+
+    public void LoadNextScene()
+    {
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+}

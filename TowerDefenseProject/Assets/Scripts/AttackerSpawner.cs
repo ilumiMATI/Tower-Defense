6df2d5b998eb9115a9f4f5efd9565b9806d18742
Mateusz Antczak <mati98848@gmail.com>
2020-09-19 01:03:30 +0200

    Attackers and spawners

---
+++ TowerDefenseProject/Assets/Scripts/AttackerSpawner.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackerSpawner : MonoBehaviour
+{
+    [SerializeField] GameObject attackerPrefab;
+
+    bool spawn = true;
+
+    // Start is called before the first frame update
+    IEnumerator Start()
+    {
+        while(spawn)
+        {
+            float randomTime = Random.Range(1f, 5f);
+            yield return new WaitForSeconds(randomTime);
+
+            Instantiate(attackerPrefab, transform.position, Quaternion.identity);
+        }
+    }
+
+}

93a71c77d03c6e9119b87ce90f448f2f502502ac
Mateusz Antczak <mati98848@gmail.com>
2020-09-23 01:29:38 +0200

    Defenders spawning

---
+++ TowerDefenseProject/Assets/Scripts/DefenderSpawner.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DefenderSpawner : MonoBehaviour
+{
+    [SerializeField] GameObject defenderPrefab;
+
+    private void OnMouseDown()
+    {
+        Debug.Log("Mouse clicked");
+        SpawnDefender(GetSquareClicked());
+    }
+
+    private Vector2 GetSquareClicked()
+    {
+        var clickPos = Input.mousePosition;
+        Vector2 worldPos = Camera.main.ScreenToWorldPoint(clickPos);
+        Vector2 gridPos = SnapToGrid(worldPos);
+        return gridPos;
+    }
+
+    private Vector2 SnapToGrid(Vector2 rawWorldPos)
+    {
+        float newX = Mathf.RoundToInt(rawWorldPos.x);
+        float newY = Mathf.RoundToInt(rawWorldPos.y);
+        return new Vector2(newX, newY);
+    }
+
+    private void SpawnDefender(Vector2 posToSpawn)
+    {
+        if(!defenderPrefab) { return; }
+
+        GameObject spawnedDefender = Instantiate(defenderPrefab, posToSpawn, Quaternion.identity);
+    }
+}

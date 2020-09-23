33c121a70045345edb46077abdced7ae0bd378b8
Mateusz Antczak <mati98848@gmail.com>
2020-09-23 10:48:08 +0200

    Score display added and fixed Ghost animations

--- TowerDefenseProject/Assets/Scripts/StarDisplay.cs
+++ TowerDefenseProject/Assets/Scripts/StarDisplay.cs
@@ -1,18 +1,36 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class StarDisplay : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] int stars = 100;
+    Text starText;
     void Start()
     {
-        
+        starText = GetComponent<Text>();
+        UpdateDisplay();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void UpdateDisplay()
     {
-        
+        starText.text = stars.ToString();
+    }
+
+    public void AddStars(int amount)
+    {
+        stars += amount;
+        UpdateDisplay();
+    }
+    public bool SpendStars(int amount)
+    {
+        if (stars >= amount)
+        {
+            stars -= amount;
+            UpdateDisplay();
+            return true;
+        }
+        return false;
     }
 }
